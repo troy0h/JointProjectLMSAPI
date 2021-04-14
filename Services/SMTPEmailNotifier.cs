4554dedcc637ea732f266fb934268a2ce56e2718
troy0h <troy0h@hotmail.co.uk>
2021-04-14 15:01:44 +0100

    Improved email sending

--- Services/SMTPEmailNotifier.cs
+++ Services/SMTPEmailNotifier.cs
@@ -5,27 +5,35 @@ using System.Security;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
-using Polly;
-using Serilog;
 using System.Configuration;
 
 namespace JointProjectLMSAPI.Services
 {
     public class SMTPEmailNotifier : IEmailNotifier
     {
+        private string Server;
+        private int Port;
+        private NetworkCredential Credentials;
+
+        public SMTPEmailNotifier()
+        {
+            Server = "smtp.gmail.com";
+            Port = 587;
+            Credentials = new NetworkCredential(ConfigurationManager.AppSettings["Email"],
+                                                ConfigurationManager.AppSettings["Password"]);
+        }
+
         public async Task SendEmailAsync(string Sender, string Recipient, string Subject, string Body)
         {
-            SmtpClient smtpClient = new("smtp.gmail.com")
+            using (SmtpClient client = new SmtpClient(Server))
             {
-                Port = 587,
-                Credentials = new NetworkCredential(ConfigurationManager.AppSettings["Email"],
-                                                    ConfigurationManager.AppSettings["Password"]),
-                EnableSsl = true,
-            };
+                client.Port = Port;
+                client.Credentials = Credentials;
+                client.EnableSsl = true;
 
-            MailMessage message = new MailMessage(Sender, Recipient, Subject, Body);
-
-            await smtpClient.SendMailAsync(message);
+                MailMessage message = new MailMessage(Sender, Recipient, Subject, Body);
+                await client.SendMailAsync(message);
+            }            
         }
     }
 }
